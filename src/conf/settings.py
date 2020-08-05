2e05dd863cb57eb58f73c99af31babd428fee761
vitorgamer58 <vitorgamer58@outlook.com>
2020-08-04 22:39:16 -0300

    BITCOIN e 404
    Diferencia o código 404 (não foi encontrado este código) de outros códigos, que podem representar o servidor fora do ar.
    
    Comando para solicitação do preço do Bitcoin com a API da Biscoint
    
    Mensagem de boas vindas

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -6,3 +6,4 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 BASE_API_URL = os.getenv("BASE_API_URL")
+BISCOINT = os.getenv("BISCOINT_API_URL")
--- src/core.py
+++ src/core.py
@@ -2,19 +2,18 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 import requests
 import os
-
-from conf.settings import BASE_API_URL, TELEGRAM_TOKEN
-
+from conf.settings import BASE_API_URL, TELEGRAM_TOKEN, BISCOINT
 
 def start(bot, update):
-    response_message = "=^._.^="
     bot.send_message(
         chat_id=update.message.chat_id,
-        text=response_message
+        text="Olá, eu sou um robô, meus comandos são:"
+        "\n"
+        "/price + Código da ação (Responde com o valor da ação)"
+        "/bitcoin (responde com a cotação do bitcoin na biscoint)"
     )
 
-
-def http_cats(bot, update, args):
+def funpricestock(bot, update, args):
     busca = BASE_API_URL + args[0]
     json = requests.get(busca)
     if(json.status_code==200):
@@ -26,39 +25,49 @@ def http_cats(bot, update, args):
             chat_id=update.message.chat_id,
             text=f"O preço da ação {symbol} é: {priceaction}, sendo a variação no dia de {changeaction}%")
     else:
+        if(json.status_code==404):
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text=f"Código {args[0]} não encontrado, tem certeza que está correto?")
+        else:
+            bot.send_message(
+                chat_id=update.message.chat_id,
+                text="O servidor das cotações está indisponível no momento")
+
+def funbitcoin(bot, update):
+    buscabtc = BISCOINT
+    jsonbtc = requests.get(buscabtc)
+        jsonbtc = jsonbtc.json()
+        pricebtc = jsonbtc['data']['last']
         bot.send_message(
             chat_id=update.message.chat_id,
-            text=f"código {args[0]} não encontrado, tem certeza que está correto?")
-
-
+            text=f"O preço do Bitcoin é R$ {pricebtc}"
+        )
 
 def unknown(bot, update):
-    response_message = "Não Entendi"
     bot.send_message(
         chat_id=update.message.chat_id,
-        text=response_message
+        text="Não Entendi"
     )
-
 
 def main():
     updater = Updater(token=TELEGRAM_TOKEN)
-
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(
         CommandHandler('start', start)
     )
     dispatcher.add_handler(
-        CommandHandler('price', http_cats, pass_args=True)
+        CommandHandler('price', funpricestock, pass_args=True)
+    )
+    dispatcher.add_handler(
+        CommandHandler('bitcoin', funbitcoin, pass_args=False)
     )
     dispatcher.add_handler(
         MessageHandler(Filters.command, unknown)
     )
-
     updater.start_polling()
 
     updater.idle()
-
 
 if __name__ == '__main__':
     print("press CTRL + C to cancel.")
