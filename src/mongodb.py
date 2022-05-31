4daad33b3cfd57b051484c791c200a106871ef22
Vitor Araujo <vitorgamer58@outlook.com>
2022-05-30 21:58:26 -0300

    Refactor

--- src/mongodb.py
+++ src/mongodb.py
@@ -2,28 +2,27 @@ from conf.settings import MONGODB
 import pymongo
 
 """ MongoDB
-    Database name: client
-    collection name: clients """
+    Database name: clients_database
+    collection name: clients_collection """
+
 
 class databaseClient:
     mongodburl = MONGODB
 
     def __init__(self):
-        self.client = pymongo.MongoClient(self.mongodburl)
-        self.db = self.client.clients_database.clients_collection
+        self.db= pymongo.MongoClient(self.mongodburl).clients_database.clients_collection
 
     def addTelegramClient(self, chat_id):
         clientAlreadyExist = self.db.find_one({"chat_id": chat_id})
-        if(clientAlreadyExist): return False
+        if(clientAlreadyExist):
+            return False
         clientAdded = self.db.insert_one({"chat_id": chat_id}).inserted_id
         return True
-    
+
     def removeTelegramClient(self, chat_id):
-        try:
-            self.db.delete_one({"chat_id": chat_id})
-            return True
-        except: return False
-    
+        self.db.delete_one({"chat_id": chat_id})
+        return True
+
     def getAllClients(self):
         clients = self.db.find({})
         return clients
